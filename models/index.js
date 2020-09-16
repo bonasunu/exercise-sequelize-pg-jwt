bec725cd66e1d13c96bc9dbf621534dbf6f71370
bonasunu <bonaventurasunu@gmail.com>
2020-09-16 19:55:53 +0700

    create 3 tables using models

--- models/index.js
+++ models/index.js
@@ -9,18 +9,52 @@ const sequelize = new Sequelize(config.DB, config.USER, config.PASS, {
 class Player extends Model {}
 Player.init(
   {
-    player_name: DataTypes.STRING,
+    player_name: { type: DataTypes.STRING, allowNull: false },
   },
-  { sequelize, modelName: 'player' }
+  { sequelize, modelName: 'user_game' }
+)
+
+class PlayerBio extends Model {}
+PlayerBio.init(
+  {
+    username: { type: DataTypes.STRING, allowNull: false },
+    first_name: { type: DataTypes.STRING, allowNull: false, defaultValue: '' },
+    last_name: { type: DataTypes.STRING, allowNull: false, defaultValue: '' },
+    city: { type: DataTypes.STRING, allowNull: false, defaultValue: '' },
+  },
+  { sequelize, modelName: 'user_game_biodata' }
+)
+
+class PlayerLog extends Model {}
+PlayerLog.init(
+  {
+    player: { type: DataTypes.STRING, allowNull: false },
+    level: { type: DataTypes.INTEGER, defaultValue: 1 },
+    experience: { type: DataTypes.INTEGER, defaultValue: 0 },
+    last_login: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+  },
+  { sequelize, modelName: 'user_game_history' }
 )
 
 const syncTable = async () => {
-  await sequelize.sync()
+  await sequelize.sync({ force: true })
   const harry = await Player.create({
     player_name: 'harrypotter',
   })
+  const harryBio = await PlayerBio.create({
+    username: 'harrypotter',
+    first_name: 'Harry',
+    last_name: 'Potter',
+    city: 'London',
+  })
+  const harryLog = await PlayerLog.create({
+    player: 'harrypotter',
+  })
+
   console.log(harry.toJSON())
+  console.log(harryBio.toJSON())
+  console.log(harryLog.toJSON())
 }
 syncTable()
 
-module.exports = Player
+module.exports = { Player, PlayerBio, PlayerLog }
--- routes/players.js
+++ routes/players.js
@@ -1,9 +1,19 @@
 const Router = require('express-promise-router')
 const router = new Router()
-const Players = require('../models')
+const db = require('../models')
 
 router.get('/', async (req, res) => {
-  const players = await Players.findAll()
+  const players = await db.Player.findAll()
+  res.status(200).json(players)
+})
+
+router.get('/log', async (req, res) => {
+  const players = await db.PlayerLog.findAll()
+  res.status(200).json(players)
+})
+
+router.get('/bio', async (req, res) => {
+  const players = await db.PlayerBio.findAll()
   res.status(200).json(players)
 })
 
