095f213273a6c96c11dd44a66f77381added510e
bonasunu <bonaventurasunu@gmail.com>
2020-09-17 02:54:33 +0700

    implement sequelize

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config({ path: '../.env' })
 
 const PORT = process.env.PORT
 const DB = process.env.PGDATABASE
@@ -6,6 +6,9 @@ const USER = process.env.PGUSER
 const PASS = process.env.PGPASSWORD
 const DB_PORT = process.env.PGPORT
 const HOST = process.env.PGHOST
+
+console.log(PORT)
+console.log(process.env.PORT)
 
 module.exports = {
   PORT,
